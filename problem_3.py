bfd54bd7492fabc48357a24672f939841d996e7f
Andrew Subotin <subotinandrey5@gmail.com>
2023-09-23 15:32:16 +0200

    Update problem_3.py

--- problem_3.py
+++ problem_3.py
@@ -1,25 +1,4 @@
 
-def length_of_longest_substring(s: str) -> int:
-    longest = ''
-    while s:
-        substring = ''
-        for char in s:
-            if char in substring:
-                break
-            else:
-                substring += char
-        if len(substring) > len(longest):
-            longest = substring
-        s = s.replace(s[0], '')
-    print(longest)
-    return len(longest)
-
-
-# print(length_of_longest_substring(s="abcabcbb"))
-# print(length_of_longest_substring(s="bbbbb"))
-# print(length_of_longest_substring(s="pwwkew"))
-# print(length_of_longest_substring(s="dvdf"))
-
 
 def length_of_longest_substring(s: str):
     longest = ''
